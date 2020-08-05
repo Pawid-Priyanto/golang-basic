8c130f817f6cc0dacd0b87cb379fca31b10d20b2
Pawid-prianto27 <pawitanto27@gmail.com>
2020-08-05 15:21:48 +0700

    update tugas appparkir

--- tugas.go
+++ tugas.go
@@ -8,112 +8,101 @@ import (
 	"github.com/rs/xid"
 )
 
-func generateIdParkir() (string, time.Time, string, string) {
+type TiketMasuk struct {
+	id   string
+	time time.Time
+}
+
+func generateIdParkir() (string, time.Time) {
 	id := xid.New().String()
 	time := time.Now()
-	var (
-		tipe, plat string
-	)
-	fmt.Print("Tipe : ")
-	fmt.Scanf("%s", &tipe)
-	fmt.Print("Plat Nomor : ")
-	fmt.Scanf("%s", &plat)
 
-	return id, time, tipe, plat
+	return id, time
 }
+func main() {
+	var kendaraan []TiketMasuk
+	menu := false
+	input := 0
 
-type Karcis struct {
-	id   string
-	plat string
-	time time.Time
-	tipe string
-}
+	for !menu {
+		fmt.Println("Welcome to Aplikasi Parkir")
+		fmt.Println("1. Parkir Masuk ")
+		fmt.Println("2. Parkir Keluar")
+		fmt.Println("3. Keluar")
+		fmt.Println("Masukkan Angka pilihan: ")
 
-// fungsi untuk membaca inputan
-func readLine(teks string) string {
-	fmt.Println(teks)
-	var input string
-	fmt.Scanln(&input)
-	return input
-}
+		fmt.Scan(&input)
 
-func ParkirMasuk() {
+		switch input {
+		case 1:
 
-	var kendaraan []Karcis
-	id, time, tipe, plat := generateIdParkir()
-	data := Karcis{id, plat, time, tipe}
+			id, time := generateIdParkir()
+			data := TiketMasuk{id, time}
 
-	kendaraan = append(kendaraan, data)
-	for i := 0; i < len(kendaraan); i++ {
-		fmt.Print("========================== \n")
-		fmt.Println("ID Parkir :", kendaraan[i].id)
-		fmt.Println("Lama Parkir :", kendaraan[i].time.Second(), "Detik")
-		fmt.Println("Nomor Plat Parkir :", kendaraan[i].plat)
-		fmt.Println("Jenis Kendaraan :", kendaraan[i].tipe)
-		fmt.Print("========================== \n")
-	}
-}
+			kendaraan = append(kendaraan, data)
+			for i := 0; i < len(kendaraan); i++ {
+				fmt.Print("========================== \n")
+				fmt.Println("ID Parkir :", kendaraan[i].id)
+				fmt.Println("Waktu Masuk :", kendaraan[i].time)
+				fmt.Print("========================== \n")
+			}
+			break
+		case 2:
+			var (
+				idBaru        string
+				platNomor     string
+				tipeKendaraan string
+			)
+			now := time.Now()
+			fmt.Print("========================== \n")
+			fmt.Print("Tipe Kendaraan : ")
+			fmt.Scan(&tipeKendaraan)
+			fmt.Print("Plat Nomor : ")
+			fmt.Scan(&platNomor)
+			fmt.Print("ID Parkir : ")
+			fmt.Scan(&idBaru)
 
-func ParkirKeluar() {
-	var kendaraan []Karcis
-	var (
-		idBaru        string
-		platNomor     string
-		tipeKendaraan string
-	)
-	now := time.Now()
-	fmt.Print("========================== \n")
-	fmt.Print("Tipe Kendaraan : ")
-	fmt.Scan(&tipeKendaraan)
-	fmt.Print("Plat Nomor : ")
-	fmt.Scan(&platNomor)
-	fmt.Print("ID Parkir : ")
-	fmt.Scan(&idBaru)
-	fmt.Print("========================== \n")
-
-	for i := 0; i < len(kendaraan); i++ {
-		if idBaru == kendaraan[i].id && tipeKendaraan == kendaraan[i].tipe && platNomor == kendaraan[i].plat {
-			waktu := now.Sub(kendaraan[i].time)
-			fmt.Println(waktu)
-			if kendaraan[i].tipe == "Motor" {
-				if waktu > 1 {
-					fmt.Print("Bayar parkir sebanyak : ")
-					fmt.Println("Rp.", int((waktu-1)*3000+5000))
+			for i := 0; i < len(kendaraan); i++ {
+				if idBaru == kendaraan[i].id {
+					waktu := int(now.Sub(kendaraan[i].time).Seconds())
+					fmt.Println("Lama parkir anda :", waktu, "Detik")
+					if tipeKendaraan == "Mobil" {
+						if waktu > 1 {
+							fmt.Print("Total Biaya Parkir : ")
+							fmt.Println("Rp.", int((waktu-1)*3000+5000))
+							fmt.Print("========================== \n")
+						} else {
+							fmt.Print("Total Biaya Parkir")
+							fmt.Println("Rp.", 5000)
+							fmt.Print("========================== \n")
+						}
+					} else if tipeKendaraan == "Motor" {
+						if waktu > 1 {
+							fmt.Print("Total Biaya Parkir : ")
+							fmt.Println("Rp.", int((waktu-1)*2000+3000))
+							fmt.Print("========================== \n")
+						} else {
+							fmt.Print("Total Biaya Parkir : ")
+							fmt.Println("Rp.", 3000)
+							fmt.Print("========================== \n")
+						}
+					}
+					kendaraan = append(kendaraan[:i], kendaraan[i+1:]...)
 				} else {
-					fmt.Print("Bayar parkir sebanyak")
-					fmt.Println("Rp.", 5000)
-				}
-			} else if kendaraan[i].tipe == "Mobil" {
-				if waktu > 1 {
-					fmt.Print("Bayar parkir sebanyak : ")
-					fmt.Println("Rp.", int((waktu-1)*2000+3000))
-				} else {
-					fmt.Print("Bayar parkir sebanyak")
-					fmt.Println("Rp.", 3000)
+					fmt.Println("Id yg anda masukan salah")
 				}
 			}
-			kendaraan = append(kendaraan[:i], kendaraan[i+1:]...)
+
+			break
+		case 3:
+			menu = true
+			fmt.Println("Terimakasih")
+			break
+		default:
+			fmt.Println("Maaf salah inputan")
+			break
 		}
 
-	}
-
-}
-
-func main() {
-
-	menu := 0
-	for menu != 99 {
-		fmt.Println("Welcome to Aplikasi Parkir")
-		pilihan := readLine(" \n 1. Parkir Masuk \n 2. Parkir Keluar \n 99. Keluar")
-		fmt.Println("Pilhan menu: ")
-		fmt.Println(pilihan)
-		if pilihan == "1" {
-			ParkirMasuk()
-		} else if pilihan == "2" {
-			ParkirKeluar()
-		} else {
-			fmt.Println("wrong type")
-		}
 	}
 
 }
